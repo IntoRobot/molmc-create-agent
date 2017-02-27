816886b2731cbbde01bc2beaa98c9f50cbaf902c
huangguozhen <huangguozhen@outlook.com>
2017-02-27 11:23:31 +0800

    添加112.124.117.64:8088跨域.

--- main.go
+++ main.go
@@ -200,10 +200,10 @@ func main() {
 
 			socketHandler := wsHandler().ServeHTTP
 
-            extraOriginStr := "https://api.intorobot.com, http://api.intorobot.com, http://www.intorobot.com, https://www.intorbot.com, http://112.124.117.64, http://localhost"
+            extraOriginStr := "https://api.intorobot.com, http://api.intorobot.com, http://www.intorobot.com, https://www.intorbot.com, http://112.124.117.64, http://112.124.117.64:8088, http://localhost"
 
 			for i := 8990; i < 9001; i++ {
-                extraOriginStr = extraOriginStr + ", http://localhost:" + strconv.Itoa(i) + ", https://localhost:" + strconv.Itoa(i) + ", http://112.124.117.64:" + strconv.Itoa(i)
+                extraOriginStr = extraOriginStr + ", http://localhost:" + strconv.Itoa(i) + ", https://localhost:" + strconv.Itoa(i)
 			}
 
 			r.Use(cors.Middleware(cors.Config{
